8491cf8c0951cc93df3b1a1849fa743867629593
Alex <alexlitvinenko123@gmail.com>
2026-02-25 19:32:14 +0100

    added additional check after login

--- tests/loginPage.spec.ts
+++ tests/loginPage.spec.ts
@@ -7,7 +7,7 @@ test.describe('Login suite', () => {
         await page.goto('/');
     })
 
-    test('Login with valid credentials', async ({ page }) => {
+    test('Login with valid credentials as Admin', async ({ page }) => {
         const loginPage = new LoginPage(page);
 
         await test.step('Check login page', async () => {
@@ -17,37 +17,23 @@ test.describe('Login suite', () => {
             await expect(loginPage.loginEmailInput).toHaveAttribute('placeholder', 'your@email.com');
             await expect(loginPage.loginPassInput).toBeEnabled();
             await expect(loginPage.loginPassInput).toHaveAttribute('placeholder', 'Введіть пароль');
-
         });
 
         await test.step('Fill Login page with valid data', async () => {
             await loginPage.loginEmailInput.fill('admin@demo.com');
             await expect(loginPage.loginEmailInput).toHaveValue('admin@demo.com');
-            await loginPage.loginEmailInput.fill('admin123');
+            await loginPage.loginPassInput.fill('admin123');
             await expect(loginPage.loginPassInput).toHaveValue('admin123');
             await loginPage.loginSubmitButton.click();
-
         });
 
         await test.step('Verify user is logged in', async () => {
-
+            await expect(loginPage.userMenu).toBeVisible();
+            await expect(loginPage.userMenu).toHaveText('Admin Demo');
+            await expect(loginPage.appTitle.first()).toContainText('FinanceManager');
+            await expect(loginPage.appLogo).toBeVisible();
         });
 
     });
 
 });
-
-test('Login with valid email and password', async ({ page }) => {
-    await test.step('Step 3: Verify result', async () => {
-        // Arrange
-
-        // Act
-
-        // Assert
-    });
-    await page.getByTestId('login-email-input')
-        .fill('admin@demo.com');
-    await page.getByTestId('login-password-input').fill('admin123');
-    await page.getByTestId('login-submit-button').click();
-});
-})
