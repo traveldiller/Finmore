55a34f7ea0d76787ebde6ce07c8831ea0893c1dc
Alex <alexlitvinenko123@gmail.com>
2026-02-27 18:12:01 +0100

    added login

--- tests/loginPage.spec.ts
+++ tests/loginPage.spec.ts
@@ -1,14 +1,16 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
 
+
 test.describe('Login suite', () => {
+    let loginPage: LoginPage;
 
     test.beforeEach(async ({ page }) => {
-        await page.goto('/');
+        loginPage = new LoginPage(page);
+        loginPage.openHomepage();
     })
 
     test('Login with valid credentials as Admin', async ({ page }) => {
-        const loginPage = new LoginPage(page);
 
         await test.step('Check login page', async () => {
             await expect(loginPage.loginSubmitButton).toBeEnabled();
@@ -20,12 +22,20 @@ test.describe('Login suite', () => {
         });
 
         await test.step('Fill Login page with valid data', async () => {
-            await loginPage.loginEmailInput.fill('admin@demo.com');
-            await expect(loginPage.loginEmailInput).toHaveValue('admin@demo.com');
-            await loginPage.loginPassInput.fill('admin123');
-            await expect(loginPage.loginPassInput).toHaveValue('admin123');
-            await loginPage.loginSubmitButton.click();
+            // await loginPage.loginEmailInput.fill('admin@demo.com');
+            // await expect(loginPage.loginEmailInput).toHaveValue('admin@demo.com');
+            // await loginPage.loginPassInput.fill('admin123');
+            // await expect(loginPage.loginPassInput).toHaveValue('admin123');
+            // await loginPage.loginSubmitButton.click();
+
+        await loginPage.login({
+            email: 'admin@demo.com',
+            password: 'admin123'
         });
+        });
+
+
+        //await loginPage.clickOnLoginButton();
 
         await test.step('Verify user is logged in', async () => {
             await expect(loginPage.userMenu).toBeVisible();
